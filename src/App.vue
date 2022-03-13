275a75d0906455b229e90f04d4101d9f95958049
zhanghong@1125541810977014.onaliyun.com <w_zhanghong1@xiwang.com>
2022-03-13 14:14:21 +0800

    feat: 登录

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <van-popup v-model="isShow">
+      <van-field
+        v-model="username"
+        error
+        required
+        label="用户名"
+      ></van-field>
+      <van-field
+        v-model="phone"
+        required
+        label="手机号"
+        placeholder="请输入手机号"
+      ></van-field>
+      <van-button type="primary" block @click="login" color="#e02727" style="borderRadius: 20px">登录</van-button>
+      <van-button type="primary" block @click="hh = !hh" style="borderRadius: 20px">出现</van-button>
+      <HelloWorld v-if="hh" />
+    </van-popup>
   </div>
 </template>
 
@@ -12,11 +27,45 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  props: {
+    LoginId: {
+      type: [Number, String],
+      required: true
+    },
+    show: {
+      type: Boolean,
+      required: false,
+      default:true
+    }
+  },
+  computed: {
+    isShow: {
+      set () {},
+      get () {
+        return this.show
+      }
+    }
+  },
+  data: () => {
+    return {
+      username: '',
+      phone: '',
+      hh: false
+    }
+  },
+  methods: {
+    login () {
+      console.log(this.username, this.phone)
+      this.$emit('update:show', false)
+      console.log(this.isShow)
+
+    }
   }
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { Field, Button, Popup } from 'vant'
+// 引入 flexible 用于设置 rem 基准值
+import 'lib-flexible/flexible.js'
 
 Vue.config.productionTip = false
+Vue.use(Field)
+Vue.use(Button)
+Vue.use(Popup)
 
 new Vue({
   render: h => h(App),
